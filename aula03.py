fc0abbeed2237e47b06d68c642e8e4b261aec358
Igor Araújo <igoritoaraujo223@gmail.com>
2026-01-28 13:13:23 -0300

    save

---
+++ aula03.py
@@ -0,0 +1,30 @@
+#Variável e tipo de dado
+#Iniciar sempre com letra minúscula;
+#O nome pode ser composto por letras, algarismos e underline;
+#E não podemos usar palavras reservadas do Python, como if, for, from, etc.
+
+
+#Tipos de dados
+
+inteiro = 8 
+
+decimal = 8.1
+
+complexo = 5+2j
+#números complexos engenhaira, física quântica, número real + número imaginário
+
+#string | tipo textual
+simples = 'Gosto de brigadeiro'
+
+duplas = "Gosto de brigadeiro"
+
+triplas = '''Gosto 
+de 
+brigadeiro''' 
+# geralmente usada quando a string tem mais de uma linha.
+
+#boolean
+verdadeiro = True
+falso = False
+
+
